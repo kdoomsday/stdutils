72496b4719ed6686b0de660716bb656950b7c1ad
Eduardo Barrientos <kdoomsday@gmail.com>
2016-04-08 20:08:15 -0430

    Poner a funcionar inicialmente Seq as a Typeclass

--- src/main/scala/ebarrientos/stdutils/adts/seq/LinkedSeq.scala
+++ src/main/scala/ebarrientos/stdutils/adts/seq/LinkedSeq.scala
@@ -1,24 +1,13 @@
 package ebarrientos.stdutils.adts.seq
 
-sealed trait LinkedSeq[+A]
-object EmptySeq extends LinkedSeq[Nothing]
-case class NodeSeq[A](a: A, s: LinkedSeq[A]) extends LinkedSeq[A]
+trait LL[A]
+case class Node[A](head: A, tail: LL[A]) extends LL[A]
+case class Empty[A]() extends LL[A]
 
-
-object LinkedSeq {
-  implicit def linkedSeqLike[A](): SeqLike[A, LinkedSeq] = new SeqLike[A, LinkedSeq] {
-    def head(s: LinkedSeq[A]): A = s match {
-      case EmptySeq => throw new Exception("Head of empty seq")
-      case NodeSeq(a, _)  => a
-    }
-
-    def tail(s: LinkedSeq[A]): LinkedSeq[A] = s match {
-      case EmptySeq      => EmptySeq
-      case NodeSeq(_, t) => t
-    }
-
-    def prepend(s: LinkedSeq[A], a: A): LinkedSeq[A] = NodeSeq(a, s)
-
-    def append(s: LinkedSeq[A], a: A): LinkedSeq[A] = ???
+object LL {
+  implicit def LLTCSeq[A]: TCSeq[A, LL] = new TCSeq[A, LL] {
+    def prepend(seq: LL[A], a: A): LL[A] = Node(a, seq)
   }
+
+  def apply[A](): LL[A] = Empty()
 }
--- src/main/scala/ebarrientos/stdutils/adts/seq/Seq.scala
+++ src/main/scala/ebarrientos/stdutils/adts/seq/Seq.scala
@@ -2,24 +2,12 @@ package ebarrientos.stdutils.adts.seq
 
 import scala.language.higherKinds
 
-trait SeqLike[A, M[_]] {
-  def head(s: M[A]): A
-  def tail(s: M[A]): M[A]
-
-  def prepend(s: M[A], a: A): M[A]
-  def append(s: M[A], a: A): M[A]
+trait TCSeq[A, Repr[_]] {
+  def prepend(seq: Repr[A], a: A): Repr[A]
 }
 
-object SeqLike {
-  implicit class SeqLikeOps[A, M[_]](sm: M[A])
-    (implicit s: SeqLike[A, M])
-  {
-    def head = s.head(sm)
-    def tail = s.tail(sm)
-
-    def prepend(a: A) = s.prepend(sm, a)
-    def append(a: A) = s.prepend(sm, a)
+object TCSeq {
+  implicit class TCSeqOps[A, Repr[_]](seq: Repr[A])(implicit ev: TCSeq[A, Repr]) {
+    def prepend(a: A): Repr[A] = ev.prepend(seq, a)
   }
-
-  def apply[A]() = EmptySeq
 }
