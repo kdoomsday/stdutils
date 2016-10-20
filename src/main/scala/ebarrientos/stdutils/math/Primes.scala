a6fce2a05f719b94f5db430e8f92dec512591835
Eduardo Barrientos <kdoomsday@gmail.com>
2016-10-19 23:53:00 -0400

    Add an isPrime function

--- src/main/scala/ebarrientos/stdutils/math/Primes.scala
+++ src/main/scala/ebarrientos/stdutils/math/Primes.scala
@@ -1,6 +1,16 @@
 package ebarrientos.stdutils.math
 
 object Primes {
-  def isPrime(i: Long): Boolean =
-    false // TODO
+  import scala.math.sqrt
+
+  def isPrime(i: Long): Boolean = {
+    if (i < 2) false
+    else if (i == 2 || i == 3) true
+    else {
+      val top = sqrt(i.toDouble).toInt
+      (2 to top).forall(i % _ != 0)
+    }
+  }
+
+  def primeSieve(top: Long): Seq[Long] = ???
 }
